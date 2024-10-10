bfec29a0cd8737338507341a43957844dfa79f72
Umer <umer.jusani.04@gmail.com>
2024-10-11 01:33:08 +0500

    little bit more syling in primary navigation.

--- ProjectOne/main.js
+++ ProjectOne/main.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
