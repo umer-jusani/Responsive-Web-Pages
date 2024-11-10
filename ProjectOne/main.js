d5bb50e5fb706adda3ce4a6e49c1ef338d6a44da
Umer <umer.jusani.04@gmail.com>
2024-11-10 22:57:16 +0500

    complete carousel section and hero section

--- ProjectOne/main.js
+++ ProjectOne/main.js
@@ -6,3 +6,46 @@ navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  // adaptiveHeight: true,
+  dots: false,
+  responsive: {
+    320: {
+      slidesToShow: 1,
+      arrows: false,
+    },
+    640: {
+      slidesToShow: 2,
+      arrows: false,
+    },
+    1024: {
+      slidesToShow: 2,
+      dots: false, // dots enabled 1280px and up
+    },
+    1200: {
+      slidesToShow: 3,
+      dots: false, // dots enabled 1280px and up
+      // disable: true
+    },
+  },
+});
+
+function updateClassBasedOnScreenSize() {
+  const box = document.getElementById("desktop-flow");
+
+  if (window.innerWidth >= 800) {
+    // box.classList.remove("small-box");
+    box.style.setProperty("--flow-spacer", "3rem");
+    box.classList.add("flow");
+  } else {
+    box.style.setProperty("--flow-spacer", "0.8rem"); // Set custom CSS variable
+    box.classList.add("flow");
+    // box.classList.add("small-box");
+  }
+}
+
+// Run on page load
+updateClassBasedOnScreenSize();
+
+window.addEventListener("resize", updateClassBasedOnScreenSize);
